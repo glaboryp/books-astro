d32d32eeaa20ac85dfe5da6674ed885b5dbd18df
Gloria <glabory@gmail.com>
2024-03-24 19:04:00 +0100

    data: Añadido nuevos libros

--- src/data/books.js
+++ src/data/books.js
@@ -1,51 +1,82 @@
 export const books = [
   {
-    id: 'clean-code',
-    title: 'Clean Code: Manual de desarrollo ágil de software',
-    description: 'En la portada de la introducción de este libro hay una imagen divertida que dice: ‘The only valid               measurement of code quality is WTFs/minute. Y se ven 2 puertas, una que dice ‘Goodcode’ y otra que pone ‘Badcode’, y nos hace una pregunta: ¿En qué puerta está nuestro código o nuestro equipo o empresa? Nos habla acerca de que la única manera de escoger la puerta correcta es mediante la maestría, que no es más que conocimiento y trabajo. Para escribir código limpio no solo es necesario conocer patrones y estilos de diseño de software y saberse la teoría, hay que practicarlo, hay que fallar y volver a practicarlo, solo así se podrá escribir buen código.',
-    author: 'Robert C. Martin',
-    link: 'https://www.amazon.com/Clean-Code-Handbook-Software-Craftsmanship-ebook/dp/B001GSTOAM'
+    id: 'eloquent-javascript',
+    title: 'JavaScript Eloquente',
+    description: '"JavaScript Eloquente" es un libro extraordinario que lleva a los lectores en un viaje apasionante a través del mundo de la programación en JavaScript. Con un enfoque amigable y accesible, el autor nos introduce en los conceptos fundamentales del lenguaje y nos guía hábilmente a través de ejercicios prácticos y desafiantes. Con una combinación equilibrada de teoría y práctica, el libro fomenta el pensamiento lógico y creativo, y nos enseña a desarrollar soluciones elegantes y eficientes. En resumen, "Eloquent JavaScript" es una guía imprescindible para aquellos que desean dominar este poderoso lenguaje de programación y explorar todo su potencial. El original está en inglés, pero aquí se enlaza a la traducción en español.',
+    author: 'Marijn Haverbeke y Miguel Angel Durán (traducción)',
+    link: 'https://eloquent-javascript-es.vercel.app/',
+    gratis: true
   },
   {
     id: 'aprendiendo-git',
     title: 'Aprendiendo Git',
     description: 'Un libro muy interesante para aprender Git. Mucho más allá de los comandos. Habla de buenas prácticas, estrategias de trabajo en equipo y flujos para mejorar la entrega de software. Además, está escrito por un autor español, lo que siempre es de agradecer.',
     author: 'Miguel Angel Durán',
-    link: 'https://leanpub.com/aprendiendo-git'
+    link: 'https://leanpub.com/aprendiendo-git',
+    gratis: false
+  },
+  {
+    id: 'javascript-inspirate',
+    title: 'Javascript, ¡Inspirate!',
+    description: 'Un libro de programación sencillo y divertido que muestra cómo programar en JavaScript desde cero. Es un viaje desde los abismos profundos de la duda, pasando por el pseudocódigo hasta llegar al maravilloso mundo de las peticiones AJAX. Hay muchas paradas en el camino, en las que aprenderemos todo lo que necesitas para empezar tu aventura como desarrollador Front-End con sólidas bases en JavaScript. No es necesario que tengas experiencia programando, pero si que es importante que sepas cómo se hacen páginas web estáticas, con conocimiento en HTML y CSS.',
+    author: 'Ulises Gascón',
+    link: 'https://leanpub.com/javascript-inspirate',
+    gratis: true
+  },
+  {
+    id: 'clean-code',
+    title: 'Clean Code: Manual de desarrollo ágil de software',
+    description: 'En la portada de la introducción de este libro hay una imagen divertida que dice: ‘The only valid               measurement of code quality is WTFs/minute. Y se ven 2 puertas, una que dice ‘Goodcode’ y otra que pone ‘Badcode’, y nos hace una pregunta: ¿En qué puerta está nuestro código o nuestro equipo o empresa? Nos habla acerca de que la única manera de escoger la puerta correcta es mediante la maestría, que no es más que conocimiento y trabajo. Para escribir código limpio no solo es necesario conocer patrones y estilos de diseño de software y saberse la teoría, hay que practicarlo, hay que fallar y volver a practicarlo, solo así se podrá escribir buen código.',
+    author: 'Robert C. Martin',
+    link: 'https://www.amazon.com/Clean-Code-Handbook-Software-Craftsmanship-ebook/dp/B001GSTOAM',
+    gratis: false
+  },
+  {
+    id: 'survivejs-react',
+    title: 'SurviveJS - React: De aprendiz a maestro',
+    description: 'SurviveJS te muestra cómo construir un Kanban sencillo usando React. La idea es que si puedes construir una aplicación sencilla, posiblemente puedas construir cosas más complejas después. La primera aplicación es siempre la más difícil y este libro viene en tu ayuda. El libro asume que tienes conocimientos básicos de Javascript y que sabes cómo usar npm.',
+    author: 'Juho Vepsäläinen y Raúl Expósito (traducción)',
+    link: 'https://raulexposito.com/react-from-apprentice-to-master-en-castellano.html',
+    gratis: true
   },
   {
     id: 'the-clean-coder',
     title: 'The Clean Coder: A Code of Conduct for Professional Programmers',
     description: '"The Clean Coder: Un Código de Conducta para Programadores Profesionales" es un libro excepcional que revoluciona la forma en que los programadores abordan su trabajo. Con sus ideas prácticas e inspiradoras, establece un nuevo estándar de profesionalismo en la industria del desarrollo de software. Su enfoque en la responsabilidad personal, el aprendizaje continuo y la comunicación efectiva, promueve la entrega de código limpio, la mejora constante y la colaboración en equipo. En resumen, este libro es una herramienta indispensable que empodera a los programadores para alcanzar la excelencia y contribuir al éxito de sus organizaciones.',
     author: 'Robert C. Martin',
-    link: 'https://www.amazon.com/Clean-Coder-Conduct-Professional-Programmers/dp/0137081073'
+    link: 'https://www.amazon.com/Clean-Coder-Conduct-Professional-Programmers/dp/0137081073',
+    gratis: false
   },
   {
     id: 'javascript-the-good-parts',
     title: 'JavaScript: The Good Parts',
     description: '"JavaScript: The Good Parts" es un libro imprescindible que destaca las mejores prácticas y características esenciales de JavaScript. A través de una visión concisa y perspicaz, el autor nos guía por el mundo de este lenguaje, revelando su potencial y resaltando sus aspectos más poderosos y efectivos. Con un enfoque en la simplicidad y la elegancia, el libro ofrece una guía práctica para aprovechar al máximo las partes más sólidas y confiables de JavaScript, permitiéndonos escribir un código claro y eficiente. En resumen, esta obra es una valiosa herramienta para cualquier desarrollador que desee dominar el arte de programar con JavaScript.',
     author: 'Douglas Crockford',
-    link: 'https://www.amazon.com/JavaScript-Good-Parts-Douglas-Crockford/dp/0596517742'
-  },
-  {
-    id: 'eloquent-javascript',
-    title: 'Eloquent JavaScript: A Modern Introduction to Programming',
-    description: '"Eloquent JavaScript" es un libro extraordinario que lleva a los lectores en un viaje apasionante a través del mundo de la programación en JavaScript. Con un enfoque amigable y accesible, el autor nos introduce en los conceptos fundamentales del lenguaje y nos guía hábilmente a través de ejercicios prácticos y desafiantes. Con una combinación equilibrada de teoría y práctica, el libro fomenta el pensamiento lógico y creativo, y nos enseña a desarrollar soluciones elegantes y eficientes. En resumen, "Eloquent JavaScript" es una guía imprescindible para aquellos que desean dominar este poderoso lenguaje de programación y explorar todo su potencial. El original está en inglés, pero hay una traducción al español que está muy bien.',
-    author: 'Marijn Haverbeke',
-    link: 'https://eloquentjavascript.net/'
+    link: 'https://www.amazon.com/JavaScript-Good-Parts-Douglas-Crockford/dp/0596517742',
+    gratis: false
   },
   {
     id: 'curso-python',
     title: 'Curso intensivo de Python',
     description: 'Gracias a esta trepidante y completa introducción a Python, no tardará en empezar a escribir programas, resolver problemas y desarrollar aplicaciones que funcionen. Comenzarás aprendiendo conceptos básicos de programación, como "variables", "listas", "clases" y "bucles" y practicarás la creación de código limpio con ejercicios sobre cada tema. Además, aprenderás a escribir programas interactivos y a probar su código con seguridad antes de añadirlo a un proyecto. En la segunda parte del libro, pondrás tus nuevos conocimientos en práctica con tres proyectos: un juego arcade inspirado en Space Invaders, un conjunto de visualización de datos con las útiles librerías de Python y una sencilla aplicación web que podrás desplegar en línea.',
     author: 'Eric Matthes',
-    link: 'https://amzn.eu/d/7FpqQh1'
+    link: 'https://amzn.eu/d/7FpqQh1',
+    gratis: false
   },
   {
     id: 'aprende-programar-c',
     title: 'Aprender a programar con C#',
     description: 'Aprender a programar es fundamental hoy en día y existen muchos lenguajes de programación con los cuales lo puedes hacer, pero el lenguaje de programación C# es un lenguaje que se presta para el aprendizaje ya que se pueden abordar conceptos tanto básicos como avanzados los cuales, al aprenderlos y pasar a otro lenguaje de programación, no tendrás problema alguno para migrar entre lenguajes. En esta obra te enseño a programar, pero, sobre todo, me he centrado en que aprendas los conceptos, una vez que aprendas el concepto, te resultará fácil aplicarlo sin importar la tecnología que vayas a utilizar en la creación de tus programas.',
     author: 'Héctor De León Guevara',
-    link: 'https://www.amazon.com/dp/6072952038'
+    link: 'https://www.amazon.com/dp/6072952038',
+    gratis: false
+  },
+  {
+    id: 'python-todos',
+    title: 'Python para todos',
+    description: '"Python para todos" es un libro sobre programación en Python escrito por Raúl González Duque. Se trata de un tutorial de Python adecuado para todos los niveles y que puedes descargar totalmente gratis.',
+    author: 'Raúl González Duque',
+    link: 'http://mundogeek.net/tutorial-python/',
+    gratis: true
   }
 ]
